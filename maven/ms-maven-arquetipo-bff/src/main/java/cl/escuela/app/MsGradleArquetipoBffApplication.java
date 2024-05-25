f62b84b6b828d70d78719739aa6b93e546eeaf76
juanazocarmalverde <juanazocarmalverde@gmail.com>
2024-05-25 19:13:36 -0400

    Commit for ...

--- maven/ms-maven-arquetipo-bff/src/main/java/cl/escuela/app/MsGradleArquetipoBffApplication.java
+++ maven/ms-maven-arquetipo-bff/src/main/java/cl/escuela/app/MsGradleArquetipoBffApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MsGradleArquetipoBffApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MsGradleArquetipoBffApplication.class, args);
 	}
