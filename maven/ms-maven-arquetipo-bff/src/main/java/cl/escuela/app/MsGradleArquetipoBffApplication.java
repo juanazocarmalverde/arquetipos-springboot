e6ce263b11736e2527acc73b4436dc3487bd84b9
juan azocar malverde <jazocar@gasco.cl>
2024-05-25 18:56:36 -0400

    first commit for archetypes

---
+++ maven/ms-maven-arquetipo-bff/src/main/java/cl/escuela/app/MsGradleArquetipoBffApplication.java
@@ -0,0 +1,13 @@
+package cl.escuela.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsGradleArquetipoBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsGradleArquetipoBffApplication.class, args);
+	}
+
+}
